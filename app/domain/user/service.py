078e390db74b74014d96d2580f3be24c9f6e31f5
rayaanr <rayaannsn@gmail.com>
2026-03-20 20:15:34 +0530

    feat: Remove admin user creation from UserService and add standalone script for admin user setup

--- app/domain/user/service.py
+++ app/domain/user/service.py
@@ -8,7 +8,6 @@ from app.domain.user.schema import (
     UserCredsSchema
 )
 from app.core.logger import get_logger
-from app.core.config import settings
 from app.utils.oauth2 import hash_password
 from app.enums.enums import UserRole
 
@@ -19,23 +18,6 @@ logger = get_logger(__name__)
 class UserService:
     def __init__(self, repo: UserRepository):
         self.repo = repo
-
-    async def create_admin_user(
-        self,
-        db: AsyncSession,
-    ) -> UserCredsSchema | UserOutSchema:
-        user = await self.repo.get_by_email(db, settings.ADMIN_LOGIN)
-        if user:
-            return user
-
-        data = UserCreateDBSchema(
-            name="Admin",
-            email=settings.ADMIN_LOGIN,
-            password_hash=hash_password(settings.ADMIN_PWD),
-            role=UserRole.USER,
-        )
-        new_user = await self.repo.create(db, data)
-        return new_user
     
     async def get_all(self, db: AsyncSession) -> list[UserOutSchema]:
         users = await self.repo.get_all(db)
