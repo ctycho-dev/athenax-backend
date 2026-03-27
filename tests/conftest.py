b6a8a76c65f43f6286b9c05602277fb7ebecb243
rayaanr <rayaannsn@gmail.com>
2026-03-27 16:37:37 +0530

    fix: remove unnecessary session.commit() call in client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -125,7 +125,6 @@ async def client(session_factory) -> AsyncGenerator[ClientWithEmail, None]:
         async with session_factory() as session:
             try:
                 yield session
-                await session.commit()
             except Exception:
                 await session.rollback()
                 raise
